e4a94434d1bb99be6cba0c7a1752cbd07017aab9
adesh <adesh.dangi@vetty.co>
2025-01-20 20:02:06 +0530

    understant let vs var

--- test_docs.js
+++ test_docs.js
@@ -1,1 +1,11 @@
-console.log("Start of file")
+// js file for basic testing
+
+const pi = 3.14;
+console.log(pi);
+// let , var 
+
+// let : single declare only
+// var is mutli decalre possible
+
+// let not poriorty for compiler to declare at start of file
+// var always get declared undefined first
